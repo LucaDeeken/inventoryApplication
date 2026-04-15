97a9fca4c231a1a1fa9701ea16a194436d8668a0
Luca Deeken <luca.deeken@fuchs-edv.de>
2026-04-15 10:22:01 +0200

    fix: poolVar

--- db/pool.js
+++ db/pool.js
@@ -1,12 +1,8 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-module.exports = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
